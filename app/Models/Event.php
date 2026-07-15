1d45b92f777cc1a52827ee5119e9dfce274852d5
chojinkyo <felixsgantenkz99@gmail.com>
2026-07-15 13:04:53 +0700

    task 1-2

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Storage;
 
 class Event extends Model
 {
@@ -35,23 +36,23 @@ class Event extends Model
     {
         return $this->hasMany(Order::class);
     }
-    public function getStatusAttribute() 
+    public function getStatusAttribute()
     {
         $hour_diff = $this->getHourDiff();
         if($hour_diff < 0) return "UPCOMING";
         if($hour_diff <= 3) return "ONGOING";
         return "COMPLETED";
     }
-    public function hasSales() : bool 
+    public function hasSales() : bool
     {
         return $this->has("orders");
     }
-    public function scopeUpcoming(Builder $query) 
+    public function scopeUpcoming(Builder $query)
     {
         $curr = Carbon::now();
         return $query->where("tanggal_waktu", "<", $curr->toString());
     }
-    public function scopeOngoing(Builder $query) 
+    public function scopeOngoing(Builder $query)
     {
         $from = Carbon::now();
         $next = $from->addHours(3);
@@ -64,7 +65,20 @@ class Event extends Model
         $next = $from->addHours(3);
         return $query->where("tanggal_waktu", ">", $next->toString());
     }
-    private function getHourDiff() 
+    public function getImageUrlAttribute()
+    {
+        $def_path = 'konser.jpg';
+        $img_path = $this->gambar;
+
+        $is_url = filter_var($img_path, FILTER_VALIDATE_URL);
+        if ($is_url == false) return $def_path;
+
+        $is_exists = Storage::exists($img_path);
+        if ($is_exists == false) return $def_path;
+
+        return $img_path;
+    }
+    private function getHourDiff()
     {
         $from = Carbon::parse($this->tanggal_waktu);
         $curr = Carbon::now();
